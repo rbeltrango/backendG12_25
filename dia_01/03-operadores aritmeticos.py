49b9086012e21accc1cb4c254f89f8b5be87373d
Ronald01 <rbeltrann@gmail.com>
2025-04-13 18:13:36 -0500

    operadores aretmeticos

---
+++ dia_01/03-operadores aritmeticos.py
@@ -0,0 +1,13 @@
+
+NUMERO1 = 18
+NUMERO2 = 50
+SUMA = NUMERO1 + NUMERO2
+RESTA = NUMERO1 - NUMERO2
+MULTIPLIACION = NUMERO1 * NUMERO2
+DIVISION = NUMERO1 / NUMERO2
+MODULO =  NUMERO1 % NUMERO2
+print("La suma es: ", SUMA)
+print("La resta es: ", RESTA)
+print("La multiplicacion es: ", MULTIPLIACION)
+print("La division es: ", DIVISION)
+print("El modulo es: ", MODULO)
