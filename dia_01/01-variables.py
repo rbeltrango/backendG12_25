192d5bb3d2a1b69fe2eb97613a2f1dd3a0a5f9a2
Ronald01 <rbeltrann@gmail.com>
2025-04-13 17:15:09 -0500

    declaración de los primeros archivos

---
+++ dia_01/01-variables.py
@@ -0,0 +1,24 @@
+# Esto es un comentario y siver para dar un contexto de que se hace
+
+nombre = 'ronald'
+edad = 30
+
+#variables de texto
+# se usan comillas simples o comillas dobles
+nombre='ronald'
+apellido="beltran"
+
+descripcion="""hola
+amigos como esta?"
+esto es un txo
+"""
+pi = 3.1415
+print(descripcion)
+print(nombre)
+print('a''b''c')
+
+print(type(descripcion)) #mostrará que tipo de variable es
+print(type(edad))
+print(type(pi))
+print(id(nombre))
+print(pi)
